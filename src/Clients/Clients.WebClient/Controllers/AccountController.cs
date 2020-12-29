cd632bd9ebef8fa154e36c536942c4c3f0168a22
Nicolás Snider <nicolas.snider@soulit.io>
2020-12-29 15:48:55 -0300

    portal

---
+++ src/Clients/Clients.WebClient/Controllers/AccountController.cs
@@ -0,0 +1,68 @@
+﻿using Clients.WebClient.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Clients.WebClient.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly string _authenticationUrl;
+
+        public AccountController(IConfiguration configuration)
+        {
+            _authenticationUrl = configuration.GetValue<string>("AuthenticationUrl");
+        }
+
+        [HttpGet]
+        public IActionResult Login() 
+        {
+            return Redirect(_authenticationUrl + $"?ReturnBaseUrl={this.Request.Scheme}://{this.Request.Host}/");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Connect(string access_token)
+        {
+            var token = access_token.Split('.');
+            //var base64Content = Convert.FromBase64String(token[1]);
+            string asd = "{\"nameid\":\"d7a28771-6c0c-4335-b067-0973c5860fc2\",\"email\":\"admin @mail.com\",\"unique_name\":\"nico\",\"family_name\":\"snider\",\"nbf\":1609205115,\"exp\":1609291515,\"iat\":1609205115}";
+            var user = JsonSerializer.Deserialize<AccessTokenUserInformation>(asd);
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.nameid),
+                new Claim(ClaimTypes.Name, user.unique_name),
+                new Claim(ClaimTypes.Email, user.email),
+                new Claim("access_token", access_token)
+            };
+
+            var claimsIdentity = new ClaimsIdentity(
+                claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var authProperties = new AuthenticationProperties
+            {
+                IssuedUtc = DateTime.UtcNow.AddHours(10)
+            };
+
+            await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity),
+                authProperties);
+
+            return Redirect("~/");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return Redirect("~/");
+        }
+    }
+}
